395dd84ba0b26ff3b2ec6eeab60d7b8c7d240c75
Hardeep <ootz0rz@gmail.com>
2014-05-25 04:07:39 -0400

    RCG shall be the prefix, and the prefix shall be RCG  * Added sandbox mode

---
+++ Game/Source/Game/RCGSandboxMode.cpp
@@ -0,0 +1,13 @@
+
+
+#include "Game.h"
+#include "RCGSandboxMode.h"
+
+
+ARCGSandboxMode::ARCGSandboxMode(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Game/Source/Game/RCGSandboxMode.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "RCGSandboxMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCGSandboxMode : public AGameMode
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
